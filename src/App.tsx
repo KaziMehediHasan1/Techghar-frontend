d523ccd2adcfbf1a69c3744b84b576ce7677164c
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-02-03 11:31:02 +0600

    css: add some varible

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Field, FieldGroup } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -43,6 +52,7 @@ const App = () => {
           </DialogContent>
         </form>
       </Dialog>
+      <h1 className="font-extrabold text-2xl text-green-600">Run korbo tmk</h1>
     </div>
   );
 };
---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export const CONFIG = {
+  apiUrl: import.meta.env.VITE_API_URL,
+  port: import.meta.env.VITE_PORT,
+};
