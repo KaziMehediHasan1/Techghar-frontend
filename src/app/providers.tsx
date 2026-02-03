dfd3eaac6f687074010cc48b6e76aea12c08bd73
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-02-03 20:28:48 +0600

    providers: add tanstack query provider for data fetching

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import type { ReactNode } from "react";
+
+// Cache saving time 5mins. retry means - if query not calling to got any apis data they can retry to got another 1time call to fetch data!
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    },
+  },
+});
+export const providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
