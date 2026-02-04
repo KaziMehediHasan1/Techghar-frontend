7431373e6bab0d431a7d99a59b158f2417722091
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-02-04 12:09:30 +0600

    store: add lang, cart, theme, auth - zustand store

--- src/store/index.ts
+++ src/store/index.ts
@@ -21,7 +21,26 @@ export interface ILanguageState {
   setLanguage: (lang: "en" | "bn") => void;
 }
 
+// theme related types -
 export interface ITheme {
-  theme: 'light' | 'dark';
+  theme: "light" | "dark";
   toggleTheme: () => void;
+}
+
+// cart related types -
+export interface ICartItem {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
+
+export interface ICartState {
+  cart: ICartItem[];
+  addToCart: (product: ICartItem) => void;
+  removeFromCart: (id: number) => void;
+  updateQuantity: (id: number, type: "increment" | "decrement") => void;
+  clearCart: () => void;
+  getTotalPrice: () => number;
 }
