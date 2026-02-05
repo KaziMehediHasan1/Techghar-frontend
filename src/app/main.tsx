6ce598021c8caafaff8617b652ed1fda7815d181
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-02-05 20:16:21 +0600

    docker: docker file and ignore file rebuild

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "@/index.css";
 import { router } from "@/app/router";
-import { Providers } from "@/app/providers";
+import { Providers } from "@/app/Providers";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
