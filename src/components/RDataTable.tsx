d8eba723a46c41f1cd6bf9710b9b21c5eb06c343
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-02-25 16:58:36 +0600

    table: add reusble filtering and searching also that's use for direct backend

--- src/components/RDataTable.tsx
+++ src/components/RDataTable.tsx
@@ -3,7 +3,11 @@ import {
   getCoreRowModel,
   useReactTable,
   getPaginationRowModel,
+  type ColumnFiltersState,
+  getSortedRowModel,
+  getFilteredRowModel,
   type ColumnDef,
+  type SortingState,
 } from "@tanstack/react-table";
 
 import {
@@ -16,6 +20,8 @@ import {
 } from "@/components/ui/table";
 import { DataTablePagination } from "@/components/RDataTablePagination";
 import { RDataTableViewOptions } from "@/components/RDataTableViewOptions";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 interface DashboardTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -26,17 +32,47 @@ const RDataTable = <TData, TValue>({
   columns,
   data,
 }: DashboardTableProps<TData, TValue>) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0, // Initial page
+    pageSize: 10, // Items per page
+  });
+
   // eslint-disable-next-line react-hooks/incompatible-library
   const table = useReactTable({
-    data,
+    data, // Backend theke asha data
     columns,
+    pageCount: 20, // Backend theke 'total pages' count lagbe
+    state: {
+      sorting,
+      columnFilters,
+      pagination,
+    },
+    onPaginationChange: setPagination,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    manualPagination: true, // Table ke bolcho: "Tumi nije paginate koro na, ami backend theke anchi"
+    manualFiltering: true,
+    manualSorting: true,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
-    <div className="shadow-ms shadow-dim-primary rounded-md bg-white space-y-3 p-4">
-      <RDataTableViewOptions table={table} />
+    <div className="shadow-ms shadow-dim-primary rounded-md bg-white space-y-3 p-4 w-full">
+      <section className="flex flex-row-reverse items-center gap-4 w-full ">
+        <RDataTableViewOptions table={table} />
+        <div className="flex items-center py-4 w-full ">
+          <Input
+            placeholder="Filter emails..."
+            value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("email")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+        </div>
+      </section>
       <Table>
         <TableHeader className="bg-slate-50">
           {table.getHeaderGroups().map((headerGroup) => (
