131dab3fd84d755a676ab619a984d4965454db3d
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-03-05 11:27:46 +0600

    hero: add auto play and hide button

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,13 +3,27 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "@/assets/styles/Slider.css";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import HeroImage from "@/assets/images/Hero01.png";
 
 const Hero = () => {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        // navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        breakpoints={{
+          0: {
+            navigation: false,
+          },
+          768: {
+            navigation: true,
+          },
+        }}
+        className="mySwiper"
+      >
         <SwiperSlide>
           <img src={HeroImage} alt="hero" loading="lazy" />
         </SwiperSlide>
