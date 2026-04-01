0ac2c3f5f36aac4a2f750959ab76a58b04969f2b
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-04-01 22:52:37 +0600

    fetch and show new product data in home page

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,7 +1,18 @@
-import VerticalCard from "@/components/cards/VerticalCard";
-import Heading from "@/components/Heading";
+import VerticalCard from '@/components/cards/VerticalCard';
+import Heading from '@/components/Heading';
+import type {
+  IProduct,
+  TProductApiResponse,
+} from '@/features/dashboard/product/product.types';
+import useFetch from '@/hooks/useFetch';
 
 const NewProduct = () => {
+  // fetching data from backend with query params for search and pagination
+  // const queryUrl = `/product?search=${debounce}&page=${pagination.pageIndex + 1}&limit=${pagination.pageSize}`;
+  const queryUrl = `/product/new`;
+  const { data } =
+    useFetch<TProductApiResponse<IProduct>>(queryUrl);
+  console.log('Check New data -', data);
   return (
     <section className="mt-3 space-y-3">
       <Heading
@@ -9,6 +20,7 @@ const NewProduct = () => {
         link="/new_product"
         linkName="See All New Products"
       />
+      fadfdfa
       <VerticalCard />
     </section>
   );
